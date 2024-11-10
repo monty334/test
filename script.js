2b7cdcb47a3cf4e982542e9e40216f61de37d4a1
monty <reesmonty6@gmail.com>
2024-11-10 21:03:05 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let display = document.getElementById('display');
+
+function appendNumber(number) {
+  display.value += number;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = 'Error';
+  }
+}
+
